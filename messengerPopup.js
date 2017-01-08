d5114282a6bcfef883c6e303075832a7e267dfea
Nathan Gathright <nathan.gathright@gmail.com>
2017-01-08 14:28:24 -0600

    Moved FB script load after click
    FB script doesn’t auto-load now.
    Add buttons had to be re-added with createElement instead of InnerHTML
    Added loading spinner behind FB widget
    Adjusted vertical centering in title bar

--- messengerPopup.js
+++ messengerPopup.js
@@ -1,44 +1,67 @@
 (function (document, window, undefined) {
 
   'use strict';
-
   var messengerButton = document.getElementById("messengerButton");
   var messengerEmail = messengerButton.getAttribute('href');
   var messengerPage = 'https://www.facebook.com/' + messengerButton.getAttribute('data-messengerPage');
   var messengerPopout = 'https://m.me/' + messengerButton.getAttribute('data-messengerPage');
 
-  // Create Facebook elements and hidden chat box
-  document.body.innerHTML += '<div id="messengerWrapper" class="messengerHidden"><div class="messengerTitleBar"><div class="messengerFloat--left"><a href="'+messengerPopout+'" target="_blank" onclick="OpenInNewTab()"><span class="messengerlineheight">Message Us</span></a></div><div class="messengerFloat--right"><a href="'+messengerEmail+'" ><i class="messengericon messengericon-email"></i></a><a href="'+messengerPopout+'" target="_blank" onclick="OpenInNewTab()"><i class="messengericon messengericon-popout"></i></a><a id="messengerClose" onclick="toggleVisibility();"><i class="messengericon messengericon-close"></i></a></div></div><div class="fb-page" data-href="'+messengerPage+'" data-tabs="messages" data-width="500" data-height="400" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="false"></div></div>';
-
-  //toggling the visibility of the chat widget
   function toggleVisibility(){
     document.getElementById('messengerWrapper').classList.toggle('messengerHidden');
   }
 
-  //popping out the chat widget
   function OpenInNewTab() {
     event.preventDefault();
     var screenwidth = screen.width-500;
     window.open(messengerPopout, '_blank',"toolbar=no,scrollbars=yes,resizable=yes,width=500,height=800,left="+screenwidth);
   }
 
-  // Click event handler
   messengerButton.addEventListener('click', function (event) {
-    event.preventDefault();
-    toggleVisibility();
-    if ( !document.getElementById('fb-root') ){
-      document.body.innerHTML += '<div id="fb-root"></div>';
+      event.preventDefault();
+      toggleVisibility();
+      if ( !document.getElementById('fb-root') ){
+        var fbRoot = document.createElement('div');
+        fbRoot.setAttribute('id', 'fb-root');
+        document.body.appendChild(fbRoot);
 
-      //running the Facebook SDK
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
-    }
-
+        //running the Facebook SDK
+        // (function(d, s, id) {
+        //   var js, fjs = d.getElementsByTagName(s)[0];
+        //   if (d.getElementById(id)) return;
+        //   js = d.createElement(s); js.id = id;
+        //   js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
+        //   fjs.parentNode.insertBefore(js, fjs);
+        // }(document, 'script', 'facebook-jssdk'));
+      }
   }, false);
+
+  var messengerTitleLeft = document.getElementById("messengerFloat--left");
+  var messengerTitleRight = document.getElementById("messengerFloat--right");
+
+  var messengerWrapper = document.createElement('div');
+  messengerWrapper.setAttribute('id', 'messengerWrapper');
+  messengerWrapper.setAttribute('class', 'messengerHidden');
+  messengerWrapper.innerHTML = '<div class="messengerTitleBar"><div id="messengerFloat--left"></div><div id="messengerFloat--right"><a href="'+messengerEmail+'" ><i class="messengericon messengericon-email"></i></a></div></div><div class="fb-page" data-href="'+messengerPage+'" data-tabs="messages" data-width="500" data-height="400" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="false"></div>';
+  document.body.appendChild(messengerWrapper);
+
+  var messengerPopoutLink = document.createElement('a');
+  messengerPopoutLink.setAttribute('id', 'messengerTitle');
+  messengerPopoutLink.setAttribute('href', messengerPopout);
+  messengerPopoutLink.onclick = function() {OpenInNewTab();};
+  messengerPopoutLink.innerHTML = 'Message Us';
+  messengerTitleLeft.appendChild(messengerPopoutLink);
+
+  var messengerPopoutLink = document.createElement('a');
+  messengerPopoutLink.setAttribute('id', 'messengerPopoutLink');
+  messengerPopoutLink.setAttribute('href', messengerPopout);
+  messengerPopoutLink.onclick = function() {OpenInNewTab();};
+  messengerPopoutLink.innerHTML = '<i class="messengericon messengericon-popout"></i>';
+  messengerTitleRight.appendChild(messengerPopoutLink);
+
+  var messengerClose = document.createElement('a');
+  messengerClose.setAttribute('id', 'messengerClose');
+  messengerClose.onclick = function() {toggleVisibility();};
+  messengerClose.innerHTML = '<i class="messengericon messengericon-close"></i>';
+  messengerTitleRight.appendChild(messengerClose);
 
 })(document, window);
