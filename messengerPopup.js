57ef0dece7e11bb590b460d4f4b9d7b239acf497
Nathan Gathright <nathan.gathright@gmail.com>
2017-01-08 12:02:44 -0600

    Refactored everything

--- messengerPopup.js
+++ messengerPopup.js
@@ -2,9 +2,9 @@ var body = document.getElementsByTagName('body');
 var messengerButton = document.getElementById("messengerButton");
 var messengerEmail = messengerButton.getAttribute('href');
 var messengerPage = 'https://www.facebook.com/' + messengerButton.getAttribute('data-messengerPage');
-var messengerpopout = 'https://m.me/' + messengerButton.getAttribute('data-messengerPage');
+var messengerPopout = 'https://m.me/' + messengerButton.getAttribute('data-messengerPage');
 //injecting elements into the DOM
-document.body.innerHTML += '<div id="fb-root"></div><div id="messengerWrapper" class="messengerHidden"><div class="messengerTitleBar"><a href="'+messengerpopout+'" target="_blank" onclick="OpenInNewTab()">Message Us</a><div class="messengerFloat"><a href="'+messengerEmail+'" class="icon-email"></a><a href="'+messengerpopout+'" target="_blank" class="icon-popout" onclick="OpenInNewTab()"></a><a id="messengerClose" class="icon-close" onclick="toggleVisibility();"></a></div></div><div class="fb-page" data-href="'+messengerPage+'" data-tabs="messages" data-width="500" data-height="400" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="false"></div></div>';
+document.body.innerHTML += '<div id="fb-root"></div><div id="messengerWrapper" class="messengerHidden"><div class="messengerTitleBar"><div class="messengerFloat--left"><a href="'+messengerPopout+'" target="_blank" onclick="OpenInNewTab()"><span class="messengerlineheight">Message Me</span></a></div><div class="messengerFloat--right"><a href="'+messengerEmail+'" ><i class="messengericon messengericon-email"></i></a><a href="'+messengerPopout+'" target="_blank" onclick="OpenInNewTab()"><i class="messengericon messengericon-popout"></i></a><a id="messengerClose" onclick="toggleVisibility();"><i class="messengericon messengericon-close"></i></a></div></div><div class="fb-page" data-href="'+messengerPage+'" data-tabs="messages" data-width="500" data-height="400" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="false"></div></div>';
 
 //running the Facebook SDK
 (function(d, s, id) {
@@ -25,5 +25,5 @@ function toggleVisibility(){
 function OpenInNewTab() {
   event.preventDefault();
   var screenwidth = screen.width-500;
-  window.open(messengerpopout, '_blank',"toolbar=no,scrollbars=yes,resizable=yes,width=500,height=800,left="+screenwidth);
+  window.open(messengerPopout, '_blank',"toolbar=no,scrollbars=yes,resizable=yes,width=500,height=800,left="+screenwidth);
 }
