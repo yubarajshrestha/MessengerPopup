cb4276b2ee441fa0f59ec5fe7a03dfca69d8f01a
Nathan Gathright <nathan.gathright@gmail.com>
2017-01-11 12:14:13 -0600

    Moved SVG from CSS to inline
    Fixed SVG images not appearing in Firefox.

--- messengerPopup.js
+++ messengerPopup.js
@@ -38,7 +38,7 @@
   var messengerWrapper = document.createElement('div');
   messengerWrapper.setAttribute('id', 'messengerWrapper');
   messengerWrapper.setAttribute('class', 'messengerHidden');
-  messengerWrapper.innerHTML = '<div class="messengerTitleBar"><div id="messengerFloat--left"></div><div id="messengerFloat--right"><a href="'+messengerEmail+'" ><i class="messengericon messengericon-email"></i></a></div></div><div class="fb-page" data-href="'+messengerPage+'" data-tabs="messages" data-width="500" data-height="400" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="false"></div>';
+  messengerWrapper.innerHTML = '<div class="messengerTitleBar"><div id="messengerFloat--left"></div><div id="messengerFloat--right"><a href="'+messengerEmail+'" ><svg class="messengericon" fill="none" stroke="#FFF" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>messenger-email</title><rect x="3" y="5" width="18" height="14"/><polyline points="21 5 12 13 3 5"/></svg></i></a></div></div><div class="fb-page" data-href="'+messengerPage+'" data-tabs="messages" data-width="500" data-height="400" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="false"></div>';
   document.body.appendChild(messengerWrapper);
 
   var messengerTitleLeft = document.getElementById("messengerFloat--left");
@@ -55,13 +55,13 @@
   messengerPopoutLink.setAttribute('id', 'messengerPopoutLink');
   messengerPopoutLink.setAttribute('href', messengerPopout);
   messengerPopoutLink.onclick = function() {OpenInNewTab();};
-  messengerPopoutLink.innerHTML = '<i class="messengericon messengericon-popout"></i>';
+  messengerPopoutLink.innerHTML = '<svg class="messengericon" fill="none" stroke="#FFF" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>messenger-external</title><polyline points="19 15 19 19 5 19 5 5 9 5"/><polyline points="13 5 19 5 19 11"/><line x1="18" y1="6" x2="11" y2="13"/></svg>';
   messengerTitleRight.appendChild(messengerPopoutLink);
 
   var messengerClose = document.createElement('a');
   messengerClose.setAttribute('id', 'messengerClose');
   messengerClose.onclick = function() {toggleVisibility();};
-  messengerClose.innerHTML = '<i class="messengericon messengericon-close"></i>';
+  messengerClose.innerHTML = '<svg class="messengericon" fill="none" stroke="#FFF" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>messenger-close</title><line x1="5" y1="5" x2="19" y2="19"/><line x1="19" y1="5" x2="5" y2="19"/></svg>';
   messengerTitleRight.appendChild(messengerClose);
 
 })(document, window);
